defde525dad7a3373b561f438659df35081dab54
Franziska-F <f.fouchs@gmail.com>
2022-04-16 13:45:25 +0200

    Add JS, game is working

---
+++ app.js
@@ -0,0 +1,67 @@
+let userScore = 0;
+
+let computerScore = 0;
+
+const userScoreDisplay = document.querySelector("#user-score");
+
+const computerScoreDisplay = document.querySelector("#computer-score");
+
+const resultDisplay = document.querySelector(".result");
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+function getComputersChoice() {
+  const rockPaperScissors = ["Rock", "Paper", "Scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return rockPaperScissors[randomNumber];
+}
+function lose(user, computer) {
+  computerScore++;
+  computerScoreDisplay.innerHTML = computerScore;
+  resultDisplay.innerHTML = `${computer} beats ${user}! Sally wins!`;
+}
+function win(user, computer) {
+  userScore++;
+  userScoreDisplay.innerHTML = userScore;
+  resultDisplay.innerHTML = `${user} beats ${computer}! You win!`;
+}
+function draw(user, computer) {
+  resultDisplay.innerHTML = `Draw!`;
+}
+
+function game(usersChoice) {
+  const computersChoice = getComputersChoice();
+  switch (usersChoice + computersChoice) {
+    case "RockPaper":
+    case "PaperScissors":
+    case "ScissorsRock":
+      lose(usersChoice, computersChoice);
+      break;
+    case "RockScissors":
+    case "PapeRock":
+    case "ScissorsPaper":
+      win(usersChoice, computersChoice);
+      break;
+    case "RockRock":
+    case "PaperPaper":
+    case "ScissorsScissors":
+      draw(usersChoice, computersChoice);
+      break;
+  }
+}
+
+function main() {
+  rock.addEventListener("click", function () {
+    game("Rock");
+  });
+  paper.addEventListener("click", function () {
+    game("Paper");
+  });
+  scissors.addEventListener("click", function () {
+    game("Scissors");
+  });
+}
+
+main();
